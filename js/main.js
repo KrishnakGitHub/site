3123ef1c959e30cb18b4fcb2c4a8e5b64f7d6ef5
Krishna <krishnakjee2016@gmail.com>
2024-06-25 23:24:08 +0530

    Programs added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
  // Get the current URL path
  var currentUrl = window.location.href;
-  console.log(currentUrl);
+  // console.log(currentUrl);
   
  // Get all the links in the navbar
  var links = document.getElementsByClassName('nav-link');
@@ -12,6 +12,6 @@
    if (links[i] === currentUrl && links[i].href!="#"|| currentUrl.includes(links[i].href) && links[i].href!="#") {
   // if(links[i] === './'+currentUrl.split('/')[currentUrl.split('/').length-1]){ 
      links[i].classList.add('active');
-     console.log(links[i].href);
+    //  console.log(links[i].href);
    }
  }
